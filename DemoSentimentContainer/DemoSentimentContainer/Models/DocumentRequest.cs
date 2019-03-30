cc303545e13eeb51e15b8e191332ae9f99fb638f
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2019-03-30 13:25:40 -0600

    Initial version

---
+++ DemoSentimentContainer/DemoSentimentContainer/Models/DocumentRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoSentimentContainer.Models
+{
+    public class DocumentRequest
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Language { get; set; }
+    }
+}
---
+++ DemoSentimentContainer/DemoSentimentContainer/Pages/SentimentAnalysisPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using DemoSentimentContainer.Helpers;
+using DemoSentimentContainer.Services;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DemoSentimentContainer.Pages
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SentimentAnalysisPage : ContentPage
+	{
+        public SentimentAnalysisPage()
+        {
+            InitializeComponent();
+        }
+        private async void AnalyzeButton_Clicked(object sender, EventArgs e)
+        {
+            Loading(true);
+
+            var sentiment = await TextAnalyticsService.AnalyzeText(MessageEntry.Text, LanguageEntry.Text);
+
+            if (sentiment != null)
+            {
+                ScoreLabel.Text = sentiment.score.ToString("N4");
+                ScoreLabel.TextColor = SentimentColor.Retrieve(sentiment.score);
+            }
+
+            Loading(false);
+        }
+
+        void Loading(bool mostrar)
+        {
+            indicator.IsEnabled = mostrar;
+            indicator.IsRunning = mostrar;
+        }
+    }
+}
